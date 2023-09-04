9cc333ba1b14233487ac941244c891ff8b584e25
mohamad amin <mohamadeaminn@gmail.com>
2023-09-04 23:10:51 +0400

    ezafe kardan tailwind

--- FamilyKid-Frontend/src/App.jsx
+++ FamilyKid-Frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div>taghir badi</div>
+      <div className="text-3xl font-bold underline">taghir badi</div>
     </>
   );
 }
