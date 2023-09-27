475477cc7d32050c399835c5195e2c7021c9718d
mohamad amin <mohamadeaminn@gmail.com>
2023-09-27 16:43:08 +0400

    table remove student remove teacher done

--- FamilyKid-Frontend/src/components/table/components/Rectangle.jsx
+++ FamilyKid-Frontend/src/components/table/components/Rectangle.jsx
@@ -1,18 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useUpdateDataMutation } from "../../../app/apiSlice";
 import { toast } from "react-toastify";
-import { handlers } from "browser-router/html5-history/adapter";
 
 const Rectangle = ({ data, job, name, setLoading }) => {
-  const [showTeachers, setShowTeachers] = useState(false);
+  const [showDetail, setShowDetail] = useState(false);
+  const [students, setStudents] = useState(data.students ?? []);
+  const input = useRef(null);
   const [updateData, {}] = useUpdateDataMutation(undefined);
   console.log(data);
 
+  // handleSearch for students
+  const searchHandler = (e) => {
+    setStudents(data.students.filter((s) => s.includes(e.target.value)));
+  };
+
   // handle function for teachers
-  const recHandler = (teacherName) => {
+  const recHandler = () => {
     // teacher handler
-    if (job === "teacher") {
-      try {
+    try {
+      if (job === "teacher") {
         setLoading(true);
         // cant have more than 3 teacher in a rectengle
         if (data.teachers.length < 3 && !data.teachers.find((t) => t == name)) {
@@ -28,26 +34,37 @@ const Rectangle = ({ data, job, name, setLoading }) => {
         else if (data.teachers.length >= 3) {
           toast.warn("sorry, can't have more than 3 teachers😣");
         }
-      } catch (e) {
-        alert(e.message);
-      } finally {
-        setTimeout(() => {
-          setLoading(false);
-        }, 1000);
       }
-    } else {
-      // supervisor handler: removes teacher
-      updateData({
-        ...data,
-        teachers: data.teachers.filter((t) => t !== teacherName),
-      });
+    } catch (e) {
+      alert(e.message);
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
     }
   };
 
-  useEffect(() => {
-    job === "supervisor" && setShowTeachers(true);
+  // supervisor handler: removes teacher
+  const removeTeacher = (teacherName) => {
+    updateData({
+      ...data,
+      teachers: data.teachers.filter((t) => t !== teacherName),
+    });
+  };
+  // supervisor handler: removes student
+  const removeStudent = (studentName) => {
+    updateData({
+      ...data,
+      students: data.students.filter((s) => s !== studentName),
+    });
+    setStudents((prev) => [...prev.filter((s) => s !== studentName)]);
+  };
 
-    return () => setShowTeachers(false);
+  useEffect(() => {
+    job === "supervisor" && setShowDetail(true);
+    console.log(input.current);
+
+    return () => setShowDetail(false);
   }, []);
 
   const rectangle = {
@@ -67,7 +84,7 @@ const Rectangle = ({ data, job, name, setLoading }) => {
       <div className={job === "supervisor" && "relative rectangle"}>
         <div
           onClick={recHandler}
-          className="flex flex-col max rectangle-parent cursor-pointer overflow-hidden rounded-2xl"
+          className="flex flex-col rectangle-parent cursor-pointer overflow-hidden rounded-2xl"
         >
           {/* if supervisor show all teacher inside */}
           <div
@@ -75,22 +92,71 @@ const Rectangle = ({ data, job, name, setLoading }) => {
             className="flex items-center gap-2 justify-center"
           >
             {job === "supervisor" &&
-              data.teachers.map((t) => <div>{t.toString().slice(0, 2)}</div>)}
+              data.teachers.map((t) => (
+                <div key={t}>{t.toString().slice(0, 2)}</div>
+              ))}
           </div>
           <div style={underline}></div>
         </div>
 
         {/* show teachers in that time */}
-        {showTeachers && (
-          <div className="absolute teachers min-w-[100px] top-10 rounded-lg shadow-2xl bg-cyan-100">
-            {data.teachers.map((t) => (
-              <div key={t} className="flex flex-col justify-between w-full">
-                <div className="flex justify-between">
-                  <div>{t}</div>
-                  <button onClick={() => recHandler(t)}>&#10060;</button>
+        {showDetail && (
+          <div className="absolute teachers min-w-[80px] top-10 rounded-lg bg-[#f8f8f2] shadow-2xl ">
+            {/* is there any teacher show it */}
+            {data.teachers?.length > 0 && (
+              <div>
+                <p className="text-lg font-semibold">teachers</p>
+                {/* divider */}
+                <div className="h-[1px] bg-black divider text-center" />
+                <div className="grid grid-cols-2 max-w-[133px] gap-2 py-1 mx-auto text-white/90">
+                  {data.teachers?.map((t) => (
+                    <div key={t} className="w-full">
+                      <div className="bg-[#44475a] w-9 rounded-sm relative cube">
+                        {t}
+                        <button
+                          onClick={() => removeTeacher(t)}
+                          className="remove-element"
+                        />
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            ))}
+            )}
+            {/* teachers end here */}
+
+            {/* seach students */}
+            {data.students?.length >= 1 && (
+              <input
+                className="bg-gray-300 focus:outline-none "
+                placeholder="search student"
+                onChange={(e) => searchHandler(e)}
+              />
+            )}
+
+            {/* showing all students */}
+            {data.students?.length > 0 && (
+              <div className="min-h-[150px]">
+                <p className="text-lg font-semibold">students</p>
+                {/* divider */}
+                <div className="h-[1px] bg-black divider text-center" />{" "}
+                <div className="grid grid-cols-2 max-w-[133px] gap-2 py-1 mx-auto text-white/90">
+                  {students.map((s) => (
+                    <div
+                      key={s}
+                      className="bg-[#44475a] w-9 rounded-sm relative cube"
+                    >
+                      {s.toString().slice(0, 3)}
+                      <button
+                        onClick={() => removeStudent(s)}
+                        className="remove-element"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {/* students end herer */}
           </div>
         )}
       </div>
